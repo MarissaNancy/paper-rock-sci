cf5f6ff36ca5432b8724e0977e9cbf17e973185b
Izaah Davila <izaahdavila@gmail.com>
2021-05-12 14:05:10 -0700

    made the arrays

---
+++ rockpaperscissors.js
@@ -0,0 +1,16 @@
+// VARIABLES;;;; user rock , paper, scissors 
+var hand = ["rock", "paper", "scissor"];
+
+// VARIABLES;;;;computer rock , paper, scissors 
+var comphand = ["rock", "paper", "scissor"];
+
+
+//USER TRACKER
+//wins//losses//ties
+//if win ++ 
+//iof lose -- 
+
+
+
+
+//COMPUTER the random choosing of the variables
